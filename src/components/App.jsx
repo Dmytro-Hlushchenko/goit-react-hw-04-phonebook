f4892f311c03f7a3bf3861789a3f9afe7e0b11bc
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-11-02 11:02:22 +0200

    fix-is-delete-initialState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,34 @@ import FormInput from "./FormInput";
 import ContactsList from "./ContactsList";
 import Filter from "./Filter";
 
-export const App = () => {
-    const [isContacts, setContacts] = useState([
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+const initialContacts =
+        [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]);
-    const [isFilter, setFilter] = useState('');
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },];
+
+export const App = () => {
+    const [contacts, setContacts] = useState(() => { 
+        const initialState = JSON.parse(localStorage.getItem('contactsList'));
+        return initialState || initialContacts;
+    });
+
+
+    const [filter, setFilter] = useState('');
 
     useEffect(() => {
-        localStorage.setItem('contactsList', JSON.stringify(isContacts))
-    }, [isContacts]);
+        localStorage.setItem('contactsList', JSON.stringify(contacts))
+    }, [contacts]);
 
     const onFormSubmit = data => {
-        const isExist = isContacts.some(
+        const isExist = contacts.some(
             ({ name }) => name.toLocaleLowerCase() === data.name.toLocaleLowerCase());
         
         if (isExist) {
             alert(`$This Name is already in contacts.`);
             return;
         };
-        setContacts(prevState => [...prevState, data]);
+        setContacts(prevContacts => [...prevContacts, data]);
     };
 
     const onFilterInput = value => {
@@ -32,14 +38,14 @@ export const App = () => {
     };
     
     const filterVisibleContacts = () => {
-        const lowerCaseFilter = isFilter.toLocaleLowerCase();
-        return isContacts.filter(contact => contact.name.toLocaleLowerCase().includes(lowerCaseFilter));
+        const lowerCaseFilter = filter.toLocaleLowerCase();
+        return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(lowerCaseFilter));
     };
     
     const deleteContact = deleteItemId => {
-        setContacts(isContacts.filter(item => item.id !== deleteItemId))
-    
-    };
+      setContacts(prevContacts =>
+      prevContacts.filter(item => item.id !== deleteItemId))
+   };
     
     return (
         <div>
